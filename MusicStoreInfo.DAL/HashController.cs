c6f020f840cba4d44e86a566461e215ce86bdb3e
McConderEz <kamazitun.jaykehd@gmail.com>
2024-02-24 00:36:58 +0300

    added hashController

---
+++ MusicStoreInfo.DAL/HashController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreInfo.DAL
+{
+    public class HashController
+    {
+        public string GenerateHash(string passwod)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(passwod));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public bool ValidatePassword(string password, string hashedPassword)
+        {
+            string inputHash = GenerateHash(password);
+            return inputHash.Equals(hashedPassword);
+        }
+    }
+}
