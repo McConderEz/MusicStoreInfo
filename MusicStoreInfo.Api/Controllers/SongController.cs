6ba5596d2898714dc182039f711e48e9aaabd005
McConderEz <kamazitun.jaykehd@gmail.com>
2024-05-30 18:49:39 +0300

    update song index interface

--- MusicStoreInfo.Api/Controllers/SongController.cs
+++ MusicStoreInfo.Api/Controllers/SongController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MusicStoreInfo.Api.Models;
 using MusicStoreInfo.DAL;
 using MusicStoreInfo.Domain.Entities;
 using MusicStoreInfo.Services.Services.DistrictService;
@@ -21,9 +22,60 @@ namespace MusicStoreInfo.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(int page = 1,
+                                   int? minDuration = null, int? maxDuration = null, string sortOrder = null, string searchString = null)
         {
-            return View(_service.GetAllAsync().Result);
+            var songs = _service.GetAllAsync().Result;
+
+
+            songs = Filter(minDuration, maxDuration, songs);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                songs = songs.Where(p => p.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            songs = Sort(sortOrder, songs);
+
+            const int pageSize = 10;
+            if (page < 1)
+                page = 1;
+
+            int recsCount = songs.Count;
+            var pager = new Pager(recsCount, page, pageSize);
+            int recSkip = (page - 1) * pageSize;
+            var data = songs.Skip(recSkip).Take(pageSize).ToList();
+            ViewBag.Pager = pager;
+            ViewBag.CurrentFilter = searchString;
+
+            return View(data);
+        }
+
+        private static List<Song>? Sort(string sortOrder, List<Song>? songs)
+        {
+            switch (sortOrder)
+            {
+                case "duration_asc":
+                    songs = songs.OrderBy(p => p.Duration).ToList();
+                    break;
+                case "duration_desc":
+                    songs = songs.OrderByDescending(p => p.Duration).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            return songs;
+        }
+
+        //TODO: Перенести в DAL и упростить
+        private static List<Song>? Filter(int? minDuration, int? maxDuration, List<Song> songs)
+        {
+            if (minDuration.HasValue)
+                songs = songs.Where(a => a.Duration >= minDuration.Value).ToList();
+            if (maxDuration.HasValue)
+                songs = songs.Where(a => a.Duration <= maxDuration.Value).ToList();           
+            return songs;
         }
 
         [HttpGet]
